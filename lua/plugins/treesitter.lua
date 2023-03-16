f8afec7b1c42da0981beb8daf995f0db61015d57
xiaobai <319331998@qq.com>
2023-03-16 09:46:20 +0800

    use <leader>v for incremental selection

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  keys = {
+    { "<leader>v", desc = "Increment selection" },
+    { "<c-space>", false },
+  },
+  opts = {
+    incremental_selection = {
+      keymaps = {
+        init_selection = "<leader>v",
+        node_incremental = "<leader>v",
+      },
+    },
+  },
+}
