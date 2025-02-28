c2a236a7d443f1bf72602948d31c0abb735ac5ae
xiaobai <319331998@qq.com>
2025-03-01 01:44:01 +0800

    use both fzf.lua and snacks.picker(primary)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,6 +8,6 @@ vim.opt.tabstop = 4
 vim.opt.expandtab = true
 vim.opt.clipboard = ""
 vim.opt.ttimeoutlen = 5
--- vim.g.lazyvim_picker = "fzf"
+vim.g.lazyvim_picker = "snacks"
 vim.o.cursorcolumn = true
 vim.o.shell = "/opt/homebrew/bin/fish"
